c2b2ddcbe4f64916bcc459e7454fc7af58a730cd
yarqui <108986486+yarqui@users.noreply.github.com>
2022-12-09 17:09:32 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('input#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+console.log(refs.input);
+console.log(refs.countryList);
+console.log(refs.countryInfo);
+
+function fetchCountries(name) {
+  fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(r => r.json())
+    .then(console.log);
+}
+
+console.log(fetchCountries(Ukraine));
