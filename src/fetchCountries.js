130eb93e996ef246b400944111ed587ef3e96951
yarqui <108986486+yarqui@users.noreply.github.com>
2022-12-10 15:57:26 +0200

    4

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,5 +2,5 @@ const endPoint = 'https://restcountries.com/v3.1/name/';
 const filter = '?fields=name,capital,population,flags,languages';
 
 export function fetchCountries(name) {
-  fetch(`${endPoint}${name}${filter}`);
+  return fetch(`${endPoint}${name}${filter}`);
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// 1. он інпут
+// 2. в онІнпуті спочатку фетч
+// 3. функція створення списку країн
+
 import './css/styles.css';
 import 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
@@ -10,17 +14,28 @@ const refs = {
   countryList: document.querySelector('.country-list'),
   countryInfo: document.querySelector('.country-info'),
 };
-console.log(refs.input);
-console.log(refs.countryList);
-console.log(refs.countryInfo);
 
 refs.input.addEventListener(
   'input',
-  debounce(() => {
-    onInput(refs.input.value);
-  }, DEBOUNCE_DELAY)
+  debounce(() => onInput(refs.input.value.trim()), DEBOUNCE_DELAY)
 );
 
 function onInput(inputValue) {
-  fetchCountries(inputValue);
+  if (inputValue === '') {
+    return;
+  }
+
+  fetchCountries(inputValue)
+    .then(r => {
+      if (!r.ok) {
+        throw new Error(r.status);
+      }
+      return r.json();
+    })
+    .then(console.log);
+}
+
+function createCountryList({ name, flags }) {
+  const markup = `<li><img alt=";${name}" src="${flags}" width="100px"></li>`;
+  console.log(markup);
 }
